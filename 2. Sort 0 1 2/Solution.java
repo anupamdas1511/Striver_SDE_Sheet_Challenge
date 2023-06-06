acc02036773801bb2099028f85fce0103b28a51e
anupamdas1511 <dasanupam151101@gmail.com>
2023-06-06 15:46:20 +0530

    initial commit

---
+++ 2. Sort 0 1 2/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void sort012(int[] arr)
+    {
+        //Write your code here
+        Arrays.sort(arr);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
